d9b360a2fa5b547e11a6d6dc63d29212604ea12d
grzegMaly <160776453+unsieknown@users.noreply.github.com>
2026-05-14 23:45:47 +0200

    Campaign Dto

--- backend/src/main/java/com/futurum/backend/dto/CampaignDto.java
+++ backend/src/main/java/com/futurum/backend/dto/CampaignDto.java
@@ -1,4 +1,29 @@
 package com.futurum.backend.dto;
 
+import com.futurum.backend.model.CampaignStatus;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CampaignDto {
+
+    private UUID campaignId;
+    private String campaignName;
+    private List<TownDto> towns;
+    private List<ProductDto> products;
+    private List<KeywordDto> keywords;
+    private BigDecimal bidAmount;
+    private BigDecimal campaignFund;
+    private CampaignStatus status;
+    private Integer radius;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
 }
