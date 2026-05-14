70d07b8b3d3f5917f6d0f2c0121df5b6d7fb574a
grzegMaly <160776453+unsieknown@users.noreply.github.com>
2026-05-14 21:28:25 +0200

    Campaign Template

--- backend/src/main/java/com/futurum/backend/controllers/CampaignController.java
+++ backend/src/main/java/com/futurum/backend/controllers/CampaignController.java
@@ -1,4 +1,39 @@
 package com.futurum.backend.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/campaign")
 public class CampaignController {
+
+    public void getCampaigns() {
+
+    }
+
+    public void getCampaignById() {
+
+    }
+
+    public void createCampaign() {
+
+    }
+
+    public void addProductToCampaign() {
+
+    }
+
+    public void removeProductFromCampaign() {
+
+    }
+
+    public void updateCampaign() {
+
+    }
+
+    public void deleteCampaign() {
+
+    }
 }
